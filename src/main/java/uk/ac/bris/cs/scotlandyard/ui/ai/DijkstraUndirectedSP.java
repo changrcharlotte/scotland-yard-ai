69425ad35f4db4995838e63d57003dd99ffac4aa
charlotte <charlotte_r_chang@outlook.com>
2026-04-03 01:39:30 +0100

    dijkstra

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/DijkstraUndirectedSP.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/DijkstraUndirectedSP.java
@@ -6,9 +6,7 @@ import com.google.common.graph.ImmutableValueGraph;
 import org.checkerframework.checker.units.qual.A;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 //the purpose of dijkstra is to add to our scoring functionality; keep track of how close the detectives are so that we can outrun them
 // for example, we keep track of the edge weights and compute, for every detective, which of mrX's moves are most unreachable
@@ -19,34 +17,58 @@ public class DijkstraUndirectedSP {
     ArrayList<ArrayList<ScotlandYard.Ticket>> tickTo;          // tickTo[v] = tickets to,  of shortest s->v path
     Integer distTo[];
     EndpointPair<Integer>[] edgeTo;            // edgeTo[v] = last edge on shortest s->v path
-    List<Integer> pq;                        // priority queue of vertices
+    IndexMinPQ<Integer> pq;                        // priority queue of vertices
 
-    private void dijkstraUndirectedSP(ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph, int mrXLocation, List<Integer> detLocations)
-    {
+    private void dijkstraUndirectedSP(ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph, int mrXLocation, List<Integer> detLocations) {
 //        Traverser.forGraph(graph).breadthFirst(StartingNode);
-        int numNodes = detLocations.size()+ 1;
+        int totalNodesNum = graph.nodes().size();
+        int numNodes = detLocations.size() + 1;
         tickTo = new ArrayList<ArrayList<ScotlandYard.Ticket>>();
-        distTo = new Integer[detLocations.size()+1];
-        edgeTo = new EndpointPair[numNodes];
+        distTo = new Integer[totalNodesNum];
+        edgeTo = new EndpointPair[totalNodesNum];
 
-        for (int v = 0; v < numNodes; v++)
+        for (int v = 0; v < numNodes; v++) {
             distTo[v] = Integer.MAX_VALUE;
+        }
         distTo[0] = 0; //assuming the first node is literally just mrX's location
+        //or perhaps just index based on
 
-        pq = new ArrayList<Integer>();
+        pq = new IndexMinPQ<Integer>(numNodes);
+        pq.insert(mrXLocation, distTo[0]);
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            for (EndpointPair e : graph.incidentEdges(v)) {
+                relax(e, v);
+            }
 
 
-
+        }
     }
+    private void relax (EndpointPair<Integer> e, Integer v){
 
-    private void relax(ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph, int startingNode) {
+        Integer w;
+        if (e.nodeU() == v) {
+            w = (Integer) e.nodeU();
+        } else {
+            w = (Integer) e.nodeV();
+        }
 
-        for (EndpointPair e : graph.incidentEdges(startingNode)) {
-            int w = e.to();
-            if (distTo[w] > distTo[v] + e.weight()) {
-                distTo[w] = distTo[v] + e.weight();
-                edgeTo[w] = e;
+        ImmutableSet<ScotlandYard.Transport> edgeTickets = graph.edgeValue((Integer) e.nodeU(), (Integer) e.nodeV()).orElseThrow();
+        Integer distance = edgeTickets.size();
+
+        if (distTo[w] > distTo[v] + distance) {
+            distTo[w] = distTo[v] + distance;
+            edgeTo[w] = e;
+
+            if(pq.contains(w)){
+                pq.decreaseKey(w,distTo[w]);
+            }
+            else{
+                pq.insert(w, distTo[w]);
             }
         }
     }
+
+
+
 }
